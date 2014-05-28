59e9666741fd90e1a481521bdb0dcb76fa88611b
Tomasz Melcer <liori@exroot.org>
2014-05-28 14:47:11 +0200

    hashed filenames

--- domarkdown.js
+++ domarkdown.js
@@ -3,9 +3,9 @@ var LineReaderSync = require('line-reader-sync');
 var wsd = require('websequencediagrams');
 var fs = require('fs');
 var sync = require('sync');
+var crypto = require('crypto');
 
 var filenames = process.argv.slice(2);
-var image_number = 0;
 filenames.forEach(function (filename) {
 	handle = new LineReaderSync(filename);
 	while(true) {
@@ -24,18 +24,21 @@ filenames.forEach(function (filename) {
 					diagram = diagram + line + '\n';
 				}
 			}
-			var working = true;
-			wsd.diagram(diagram, 'modern-blue', 'png', function(er, buf, typ) {
-				if (er) {
-					console.error(er);
-				} else {
-					fs.writeFile("diagram" + image_number + ".png", buf);
-					console.log('![](diagram' + image_number + '.png)');
-					image_number++;
-				}
-				working = false;
-			});
-			while(working) {require('deasync').sleep(100);}
+			var hash = crypto.createHash('md5').update(diagram).digest('hex');
+			var filename = 'diagram-' + hash + '.png';
+			if (!fs.existsSync(filename)) {
+				var working = true;
+				wsd.diagram(diagram, 'modern-blue', 'png', function(er, buf, typ) {
+					if (er) {
+						console.error(er);
+					} else {
+						fs.writeFile(filename, buf);
+					}
+					working = false;
+				});
+				while(working) {require('deasync').sleep(100);}
+			}
+			console.log('![](' + filename + ')');
 		} else {
 			console.log(line);
 		}
